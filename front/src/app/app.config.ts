41dac9bf7cf58ee092b1e0b8781d85f5cbe88025
Jérémy Munsch <github@jeremydev.ovh>
2025-01-26 13:05:25 +0100

    refactor(front): app config imports sort

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { registerLocaleData } from '@angular/common'
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import localeFr from '@angular/common/locales/fr'
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter, TitleStrategy } from '@angular/router'
 import Aura from '@primeng/themes/aura'
-import { providePrimeNG } from 'primeng/config'
-
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { provideMarkdown } from 'ngx-markdown'
 import { MessageService } from 'primeng/api'
+import { providePrimeNG } from 'primeng/config'
+
 import { routes } from './app.routes'
 import { DynamicTitleStrategy } from './core/strategies/DynamicTitleStrategy'
 import { SessionInterceptor } from './interceptor/session.interceptor'
