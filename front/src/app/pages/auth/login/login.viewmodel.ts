0f9665ad0dc809c89a4fcb0b77defac38209f128
Jérémy Munsch <github@jeremydev.ovh>
2025-01-31 16:51:37 +0100

    refactor(front): Login and Register add loading spinner

--- front/src/app/pages/auth/login/login.viewmodel.ts
+++ front/src/app/pages/auth/login/login.viewmodel.ts
@@ -3,29 +3,36 @@ import { AuthService, errors, ToastService } from '@core/services'
 
 @Injectable()
 export default class LoginViewModel {
+  public readonly formErrorMessage = signal('')
+  public readonly login = signal('login')
+  public readonly password = signal('password')
+
+  public readonly loading = signal(false)
+
   constructor(
     private authService: AuthService,
     private toastService: ToastService,
   ) {
   }
 
-  public readonly formErrorMessage = signal('')
-  public readonly login = signal('login')
-  public readonly password = signal('password')
-
   public proceedToLogin(
     login: string, password: string,
   ): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
+      // * Set loading
+      this.loading.set(true)
+      // * Login
       this.authService.login(
         { login, password },
       ).subscribe({
         complete: () => {
+          this.loading.set(false)
           this.formErrorMessage.set('')
           this.toastService.success('Connexion réussie')
           resolve(true)
         },
         error: (error) => {
+          this.loading.set(false)
           if (error instanceof errors.LoginFailure) {
             this.formErrorMessage.set('Identifiants incorrects')
             return
--- front/src/app/pages/auth/register/register.viewmodel.ts
+++ front/src/app/pages/auth/register/register.viewmodel.ts
@@ -3,13 +3,7 @@ import { AuthService, errors, ToastService } from '@core/services'
 
 @Injectable()
 export default class RegisterViewModel {
-  constructor(
-    private authService: AuthService,
-    private toastService: ToastService,
-  ) {
-  }
-
-  public formErrorMessage = signal('')
+  public readonly formErrorMessage = signal('')
   public readonly errors = {
     email: signal(''),
     username: signal(''),
@@ -20,22 +14,33 @@ export default class RegisterViewModel {
   public readonly username = signal('')
   public readonly password = signal('')
 
+  public readonly loading = signal(false)
+
+  constructor(
+    private authService: AuthService,
+    private toastService: ToastService,
+  ) {
+  }
+
   public proceedToRegister(
     email: string,
     username: string,
     password: string,
   ): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
+      this.loading.set(true)
       this.resetErrors()
       this.authService.register(
         { email, username, password },
       ).subscribe({
         complete: () => {
+          this.loading.set(false)
           this.formErrorMessage.set('')
           this.toastService.success('Inscription réussie')
           resolve(true)
         },
         error: (error) => {
+          this.loading.set(false)
           if (error instanceof errors.ValidationError) {
             this.formErrorMessage.set('Des champ n\'ont pas pu être validés')
             this.setErrors(error.getErrors())
