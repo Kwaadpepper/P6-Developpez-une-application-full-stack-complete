25741d3e69cb2ad979e6eb5302dee8ba0c5e85d4
Jérémy Munsch <github@jeremydev.ovh>
2025-02-15 11:19:41 +0100

    refactor(front): Simplify HomeViewModel

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
 import { ButtonModule } from 'primeng/button'
+import HomeViewModel from './home.viewmodel'
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, ButtonModule],
+  imports: [
+    RouterLink,
+    ButtonModule,
+  ],
+  providers: [HomeViewModel],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(
+    public readonly viewModel: HomeViewModel,
+  ) {}
 }
